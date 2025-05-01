5f97fbc067e7f8a93ea9e1a7749a9bc5617163cf
john1909m <johnemil21@yahoo.com>
2025-05-02 01:12:10 +0300

    test 4

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,19 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['react-slick', 'slick-carousel/slick/slick.css', 'slick-carousel/slick/slick-theme.css'],
+    include: ['react-slick', 'slick-carousel'],
+    force: true
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "slick-carousel/slick/slick.css"; @import "slick-carousel/slick/slick-theme.css";`
+      }
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: ['slick-carousel/slick/slick.css']
+    }
+  }
 });
