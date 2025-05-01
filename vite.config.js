abfac3c07fdba85766eb0efda322da9370962768
john1909m <johnemil21@yahoo.com>
2025-05-02 01:18:14 +0300

    testt

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base:"/",
   plugins: [react()],
   optimizeDeps: {
     include: ['react-slick', 'slick-carousel'],
