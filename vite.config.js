e9220aacbe2a1e7b004706c7e9b2a990894fde84
john1909m <johnemil21@yahoo.com>
2025-05-02 00:32:10 +0300

    test 3

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ['react-slick'],
+//   },
+//   build: {
+//     rollupOptions: {
+//       external: ['react-slick'],
+//     },
+//   },
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['react-slick'],
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-slick'],
-    },
+    include: ['react-slick', 'slick-carousel/slick/slick.css', 'slick-carousel/slick/slick-theme.css'],
   },
 });
